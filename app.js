431deb4fe25efb69b805709ea37bedd69fac24a9
figgyjay <79807654+figgyjay@users.noreply.github.com>
2022-09-13 15:33:32 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-https://expressjs.com/en/starter/hello-world.html
+const express = require('express')
+const app = express()
+const port = 3000
+const favoriteMovieList = ['Inception', 'The Color Purple', 'Coming To America', 'Rasing Arizona', 'The Pianist', 'Dune'];
+const movieString = favoriteMovieList.join(",");
+
+app.get('/my-name', (req, res) => {
+    console.log('Default Route')
+  res.send('Christopher Hoskins 9/13/2022')  
+})
+
+app.get('/list-movies', (req, res) => {
+    console.log('movielist');
+    const movieString = favoriteMovieList.join("+");
+    res.send(movieString);
+})
+
+console.log(movieString)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
