d14ae8b26e9070b0ab416ad449484f8e2594669f
figgyjay <79807654+figgyjay@users.noreply.github.com>
2022-09-19 09:29:03 -0500

    ExpressIntro Assignment 2

--- app.js
+++ app.js
@@ -1,27 +1,122 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const favoriteMovieList = ['Inception', 'The Color Purple', 'Coming To America', 'Rasing Arizona', 'The Pianist', 'Dune'];
+const favoriteMovieList = [];
 const movieString = favoriteMovieList.join(",");
+let titleToUpdate = null
+let titleToDelete = null
+
+
+
 
 app.get('/my-name', (req, res) => {
-    console.log('Default Route')
+    console.log('Default Route');
   res.send('Christopher Hoskins 9/13/2022')  
 })
 
 app.get('/list-movies', (req, res) => {
+   
+    favoriteMovieList;
     console.log('movielist');
-    const movieString = favoriteMovieList.join("+");
+   
+
+    favoriteMovieList.push(
+      {
+        title: "Batman",
+        starRating: "5",
+        isRecommended: true,
+        createAt: Date,
+        lastModified: Date
+      }
+    );
+    favoriteMovieList.push(
+      {
+        title: "Batman",
+        starRating: "5",
+        isRecommended: true,
+        createAt: Date,
+        lastModified: Date
+      }
+
+    );
+    favoriteMovieList.push(
+      {
+        title: "Batman",
+        starRating: "5",
+        isRecommended: true,
+        createAt: Date,
+        lastModified: Date
+      }
+
+    );
+    favoriteMovieList.push(
+      {
+        title: "Batman",
+        starRating: "5",
+        isRecommended: true,
+        createAt: Date,
+        lastModified: Date
+      }
+
+    );
+
+    if (isRecommended < 3) {
+        isRecommended = false;
+    };
+
+    console.log(favoriteMovieList);
+
+    const movieString = favoriteMovieList.join(",");
     res.send(movieString);
 })
+console.log(movieString);
 
-console.log(movieString)
+app.get('/new-movie', (req, res) => {
+
+  console.log('new Movie');
+  favoriteMovieList.push('Joker');
+  const movieString = favoriteMovieList.join(",");
+
+  console.log(favoriteMovieList);
+  res.send(movieString);
+})
+console.log(movieString);
+
+app.get('/all-movies', (req, res) => {
+  console.log('all movies');q
+  res.send(movieString);
+})
+console.log(newMovie);
+
+app.get('/update-movies', (req, res) => {
+  console.log(req.query);
+  titleToUpdate = req.query.titleToUpdate;
+  console.log("Title to Update");
+ 
+  res.send("Update Title" + titleToUpdate);
+})
+
+
+app.get('/delete-movies', (req, res) => {
+  console.log(req.query);
+  titleToDelete = req.query.titleToDelete;
+  console.log("Title to Delete");
+ 
+  res.send("Delete Title" + titleToDelete);
+})
+
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-
+ 
 
 
 
