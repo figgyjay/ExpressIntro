8f81cc6d12f912065aac664cc7bb3a9c5b2ae359
figgyjay <79807654+figgyjay@users.noreply.github.com>
2022-09-23 18:33:30 -0500

    ExpressIntro Assignment 2 Retry -
    My first one sucked. Just doing this again for me.

--- app.js
+++ app.js
@@ -9,104 +9,190 @@ let titleToDelete = null
 
 
 
-app.get('/my-name', (req, res) => {
-    console.log('Default Route');
-  res.send('Christopher Hoskins 9/13/2022')  
-})
+// app.get('/my-name', (req, res) => {
+//     console.log('Default Route');
+//   res.send('Christopher Hoskins 9/13/2022')  
+// })
 
-app.get('/list-movies', (req, res) => {
-   
-    favoriteMovieList;
+app.get('/all-movies', (req, res) => {
     console.log('movielist');
-   
-
-    favoriteMovieList.push(
-      {
-        title: "Batman",
-        starRating: "5",
-        isRecommended: true,
-        createAt: Date,
-        lastModified: Date
-      }
-    );
-    favoriteMovieList.push(
-      {
-        title: "Batman",
-        starRating: "5",
-        isRecommended: true,
-        createAt: Date,
-        lastModified: Date
-      }
-
-    );
-    favoriteMovieList.push(
-      {
-        title: "Batman",
-        starRating: "5",
-        isRecommended: true,
-        createAt: Date,
-        lastModified: Date
-      }
-
-    );
-    favoriteMovieList.push(
-      {
-        title: "Batman",
-        starRating: "5",
-        isRecommended: true,
-        createAt: Date,
-        lastModified: Date
-      }
-
-    );
-
-    if (isRecommended < 3) {
-        isRecommended = false;
-    };
-
-    console.log(favoriteMovieList);
-
-    const movieString = favoriteMovieList.join(",");
-    res.send(movieString);
+    res.json(movieString);
 })
-console.log(movieString);
 
-app.get('/new-movie', (req, res) => {
+    const newMovieTitle = [{
+        title: "",
+        starRating: 5,
+        isRecommended: true,
+        createAt: new Date(),
+        lastModified: new Date()
+      
+    }];
 
-  console.log('new Movie');
-  favoriteMovieList.push('Joker');
-  const movieString = favoriteMovieList.join(",");
+app.post('/new-movie:newMovieTitle', (req, res) => {
+  console.log(req.body);
 
-  console.log(favoriteMovieList);
-  res.send(movieString);
+  const newMovie = {
+    title: "",
+    starRating: 0,
+    isRecommended: false
+  }
+  newMovieTitle = req.body.title
+  newMovieTitle = req.body.starRating
+  newMovieTitle = req.body.isRecommended
+
+  if (re.body.isRecommended !== true && req.body.isRecommended !== false){
+    res.json ({
+      success: false,
+      message: " isRecommended field is required"
+    })
+    return;
+  }  
+  else {
+    newMovie.isRecommended = req.body.isRecommended
+
+  };
+
+  if (req.body.title !== true && req.body.title !== false){
+    res.json ({
+      success: false,
+      message: "field is required"
+    })
+    return;
+  }  
+  else {
+    newMovie.title = req.body.title
+
+  };
+
+  if (re.body.starRating !== true && req.body.starRating !== false){
+    res.json ({
+      success: false,
+      message: " isRecommended field is required"
+    })
+    return;
+  }  
+  else {
+    newMovie.starRating = req.body.starRating
+
+  };
+
+   console.log(newMovie)
+
+   favoriteMovieList.push(newMovie)
+
+  res.json({
+    success: true
+  })
 })
 console.log(movieString);
 
 app.get('/all-movies', (req, res) => {
-  console.log('all movies');q
-  res.send(movieString);
-})
-console.log(newMovie);
-
-app.get('/update-movies', (req, res) => {
-  console.log(req.query);
-  titleToUpdate = req.query.titleToUpdate;
-  console.log("Title to Update");
- 
-  res.send("Update Title" + titleToUpdate);
+  res.json(favoriteMovieList);
+  console.log('all movies');
 })
 
 
-app.get('/delete-movies', (req, res) => {
-  console.log(req.query);
-  titleToDelete = req.query.titleToDelete;
-  console.log("Title to Delete");
- 
-  res.send("Delete Title" + titleToDelete);
+app.put('/update-movies:titleToUpdate', (req, res) => {
+  //route parameter
+  console.log("req param", req.params);
+
+  const titleToUpdate = req.params.titleToUpdate;
+  const originalMovie = favoriteMovie.findIndex((movie)=>{
+    if  (movie.title === req.params.titleToUpdate){
+      return true
+    } else {
+      return false
+    }
+  });
+
+  // const originalMovie = favoriteMovieList[indexOfMovie];
+
+  // const updatedMovie = {
+  
+
+
+  
+  const  updatedMovie = {
+        title: originalMovie.title,
+        starRating: originalMovie.starRating,
+        isRecommended: originalMovie.isRecommended,
+        createAt: originalMovie.createAt,
+        lastModified: new Date()
+       
+  };
+
+  if(req.body.title !== undefined){
+    updatedMovie.title = req.body.title
+  }
+  if(req.body.starRating !== undefined){
+    updatedMovie.starRating= req.body.starRating
+  }
+  if(req.body.title !== undefined){
+    updatedMovie.isRecommended = req.body.isRecommended
+  }
+  return;
+
+  // newMovie.title = req.body.title
+  // newMovie.starRating = req.body.starRating
+  // newMovie.isRecommended = req.body.isRecommended
+
+  // console.log(titleToUpdate)
+  // console.log(newTitle);
+
+  // const indexOfMovie = favoriteMovieList.indexOf(titleToUpdate);
+  // console.log(indexOfMovie);
+
+  // favoriteMovieList(indexOfMovie) = newTitle
+  // console.log(favoriteMovieList);
+
+
+  res.json({
+    success: true
+  });
 })
+favoriteMovieList[indexOfMovie] = updatedMovie
 
 
+app.delete('/delete-movies:titleToDelete', (req, res) => {
+  console.log("req param", req.params);
 
+  const indexOfMovie = favoriteMovie.findIndex((movie)=>{
+    if  (movie.title === req.params.titleToDelete){
+      return true
+    } else {
+      return false
+    }
+  });
+
+  if(indexOfMovie < 0){
+    res.json({
+      hasBeenDeleted: false
+    });
+    return;
+  }
+
+  // console.log(favoriteMovieList);
+  res.json({
+    success: true
+  });
+});
+
+  favoriteMovieList.splice(indexOfMovie, 1);
+  res.json({
+    hasBeenDeleted: true
+  })
+
+  app.get('single-movie/:titleToFind', (req, res) => {
+    const titleToFind = req.params.titleToFind
+    if (movie.title === req.params.titleToFind){
+      return true
+    }else{
+      return false
+    }
+  })
+
+  const foundMovie = favoriteMovieList[foundMovieIndex];
+  res.json(foundMovie)
 
 
 
